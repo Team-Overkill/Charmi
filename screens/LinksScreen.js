b74c612dbeea7e1e6490b114911981dc2e9f305f
Paul Murff <titan.murff@gmail.com>
2017-07-18 09:01:41 -0600

    Partially working so far some funtionality 1st attempt to merge

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,19 +1,60 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, ListView } from 'react-native';
+import { List, ListItem } from 'react-native-elements'
 
+
+
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+ 
+]
 export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+constructor() {
+    super();
+    
+    
+  }
+  componentWillMount() {
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
+this.state = {
+      dataSource: ds.cloneWithRows(list),
+    };
+  }
+  static navigationOptions = {
+    title: 'Matches',
+  };
+renderRow (rowData, sectionID) {
+  return (
+    <ListItem
+      roundAvatar
+      key={sectionID}
+      title={rowData.name}
+      subtitle={rowData.subtitle}
+      avatar={{uri:rowData.avatar_url}}
+    />
+  )
+}
   render() {
     return (
       <ScrollView style={styles.container}>
-
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+ <List>
+      <ListView
+        renderRow={this.renderRow}
+        dataSource={this.state.dataSource}
+      />
+    </List>
+     
+        
 
       </ScrollView>
     );
